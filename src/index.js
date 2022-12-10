ee6c63635427d3801fa2dabfb5a865d1f0e9698c
Lex van der Woude <lex@bizhub.co.nz>
2022-12-10 19:38:24 +1300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+export default class useScanner
+{
+    constructor(success) {
+        this.success = success
+        this.timeout = null
+        this.buffer = null
+
+        this.listener = event => {
+            this.handler(event)
+        }
+        document.addEventListener('keyup', this.listener)
+    }
+
+    handler({key}) {
+        let valid = '0123456789'
+
+        if (valid.includes(key)) {
+            clearTimeout(this.timeout)
+            this.buffer = this.buffer + key
+
+            if (this.buffer.length == 13) {
+                this.success(this.buffer)
+
+                this.buffer = ''
+            }
+
+            this.timeout = setTimeout(() => {
+                this.buffer = ''
+            }, 120)
+        }
+    }
+
+    destroy() {
+        document.removeEventListener('keyup', this.listener)
+    }
+}
